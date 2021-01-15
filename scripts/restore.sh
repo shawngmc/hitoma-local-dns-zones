6be2a669a040385b9c64cf449bf40811caa43113
Shawn McNaughton <shawngmc@gmail.com>
2021-01-15 23:48:46 +0000

    fixing bind path

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Enable throw error if non-zero exit
 set -e
 
-cp $DIR/../etc/bind9/* /etc/bind9
+cp $DIR/../etc/bind9/* /etc/bind
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Enable throw error if non-zero exit
 set -e
 
-named-checkconf $DIR/../etc/bind9/named.conf.local
-named-checkzone hitoma.com $DIR/../etc/bind9/hitoma.com.db
-named-checkzone hitoma.com $DIR/../etc/bind9/hitoma.com.rev
+named-checkconf $DIR/../etc/bind/named.conf.local
+named-checkzone hitoma.com $DIR/../etc/bind/hitoma.com.db
+named-checkzone hitoma.com $DIR/../etc/bind/hitoma.com.rev
